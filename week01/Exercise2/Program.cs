357492af515209499ef5c98f9bfa820d90d97079
carlos200514 <cfrojas05@gmail.com>
2026-01-09 16:18:12 -0600

    exercise 1-2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade: ");
+        string grade= Console.ReadLine();
+        int number= int.Parse(grade);
+        if (number >= 90)
+        {
+            Console.WriteLine("Your score is: A");
+        }
+        else if (number >= 80)
+        {
+            Console.WriteLine("Your score is: B");
+        }
+        else if (number >=70)
+        {
+            Console.WriteLine("Your score is: C");
+        }
+        else if (number >= 60)
+        {
+            Console.WriteLine("Your score is: D");
+        }
+        else
+        {
+            Console.WriteLine("Your score is: F");
+        }
+
     }
 }
