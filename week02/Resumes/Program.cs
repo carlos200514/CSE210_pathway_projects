0c78ff360b4185e6ec3b3406a198e10cd91f9c9f
carlos200514 <cfrojas05@gmail.com>
2026-01-15 13:43:11 -0600

    week 2 learning act

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1= new Job();
+        job1._jobTitle= "Software engineer";
+        job1._company= "Microsoft";
+        job1._startYear= 2020;
+        job1._endYear= 2023;
+        Job job2= new Job();
+        job2._jobTitle= "Manager";
+        job2._company= "apple";
+        job2._startYear= 2022;
+        job2._endYear= 2024;
+
+        Resume ownResume= new Resume();
+        ownResume._name= "Felipe Rojas";
+        ownResume._jobs.Add(job1);
+        ownResume._jobs.Add(job2);
+        ownResume.Display();
     }
 }
