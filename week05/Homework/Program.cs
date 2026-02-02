6062b790da148bf815a6c3d9ec59e3149f719dba
carlos200514 <cfrojas05@gmail.com>
2026-02-02 10:11:27 -0600

    inheritance activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Console.WriteLine("Homework Project.");
+        Assignment assignment= new Assignment("Felipe Rojas","Fractions");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+        MathAssignment mathAssignment= new MathAssignment("Felipe Rojas", "Division", "Section 7.3", "Problems 8-12");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine();
+        WritingAssignment writingAssignment= new WritingAssignment("Felipe Rojas", "USA History", "The Causes of the Cold War");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
